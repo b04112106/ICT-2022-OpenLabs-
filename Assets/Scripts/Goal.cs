273f9a047e18bcc797d23dc8405d4cf10b2962cd
Your Name <you@example.com>
2022-01-12 17:53:21 +0800

    First

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public ParticleSystem left, right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Target")
+        {
+            left.Play();
+            right.Play();
+        }
+    }
+}
